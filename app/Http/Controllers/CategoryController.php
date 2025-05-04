3f8b9b34b4f418a2364cd4fad76848767acf2282
ahmadibni <ahmadibni007@gmail.com>
2025-05-04 23:16:14 +0700

    add resource and request,

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CategoryRequest;
+use App\Http\Resources\CategoryResource;
+use CategoryService;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    private $categoryService;
+
+    public function __construct(CategoryService $categoryService)
+    {
+        $this->categoryService = $categoryService;
+    }
+
+    public function index()
+    {
+        $fields = ['name', 'photo', 'tagline'];
+        $category = $this->categoryService->getAll($fields);
+        return response()->json(CategoryResource::collection($category));
+    }
+
+    public function show($id)
+    {
+        try {
+            $fields = ['name', 'photo', 'tagline'];
+            $category = $this->categoryService->getById($id, $fields);
+            return response()->json(new CategoryResource($category));
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'message' => 'Category not found'
+            ], 404);
+        }
+    }
+
+    public function store(CategoryRequest $request){
+        $category = $this->categoryService->create($request->validated());
+        return response()->json(new CategoryResource($category), 201);
+    }
 }

23d357a7c47618f4d6c9f8af90765b4ec7f90d5b
ahmadibni <ahmadibni007@gmail.com>
2025-05-04 16:45:44 +0700

    initial commit

---
+++ app/Models/WarehouseProduct.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class WarehouseProduct extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = ['product_id', 'warehouse_id', 'stock'];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function warehouse()
+    {
+        return $this->belongsTo(Warehouse::class);
+    }
+}
